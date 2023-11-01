3f8d372b556dfd617174a228976c5cf95211be2d
Andrew Li <Andrewli2024@gmail.com>
2023-11-01 12:11:48 -0400

    Fix build errors

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Home.scss";
 
 export default function Home() {
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./TopNav.scss";
 
 export default function TopNav() {
